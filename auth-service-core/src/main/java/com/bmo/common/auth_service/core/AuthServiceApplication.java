a66ee8a0f82454613b921f586ad4bc58419ba26d
Mark Manirko <->
2023-04-29 11:17:24 +0200

    implemented client

--- auth-service-core/src/main/java/com/bmo/common/auth_service/core/AuthServiceApplication.java
+++ auth-service-core/src/main/java/com/bmo/common/auth_service/core/AuthServiceApplication.java
@@ -3,10 +3,8 @@ package com.bmo.common.auth_service.core;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
-import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @EnableConfigurationProperties
-@EnableFeignClients
 @SpringBootApplication
 public class AuthServiceApplication {
 
